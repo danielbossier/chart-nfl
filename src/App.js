3f647f78ca3149a37228ab3d179f7db503842b8e
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-08-31 22:26:56 -0700

    Successful Test with Template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { teamState } from "react";
 import "./App.css";
 import ScatterPlot from "./Components/ScatterPlot";
-import {TeamData} from './Data'
+import { TeamData } from './Data';
 
 function App() {
-  const [teamData] = teamState({
+  const [teamData, setTeamData] = teamState({
     labels: TeamData.map((data) => data.team),
     datasets: [
       {
@@ -17,12 +17,37 @@ function App() {
       },
     ],
   });
-
+  
   return (
     <div className="App">
-      <ScatterPlot chartData={teamData}/>
+      <div style={{ width: 700 }}>
+        {/* <BarChart chartData={userData} /> */}
+      </div>
+      <div>
+        <ScatterPlot chartData={teamData}/>
+        <h1>THIS WILL BE AN NFL CHART</h1>
+      </div>
     </div>
   );
 }
 
+// function App() {
+//   const [userData, setUserData] = useState({
+//     labels: UserData.map((data) => data.year),
+//     datasets: [
+//       {
+//         label: "Users Gained",
+//         data: UserData.map((data) => data.userGain),
+//         backgroundColor: [
+//           "rgba(75,192,192,1)",
+//           "#ecf0f1",
+//           "#50AF95",
+//           "#f3ba2f",
+//           "#2a71d0",
+//         ],
+//         borderColor: "black",
+//         borderWidth: 2,
+//       },
+//     ],
+//   });
 export default App;
--- src/Components/ScatterPlot.js
+++ src/Components/ScatterPlot.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Scatter } from 'react-chartjs-2';
-// import { Chart as ChartJS } from 'chart.js/auto';
+// eslint-disable-next-line
+import { Chart as ChartJS } from 'chart.js/auto';
 
 function ScatterPlot({ chartData }) {
     return <Scatter data={chartData} />;

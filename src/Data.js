8f87b120722914d8126adea6738c5ed0b9d2b15c
danielbossier <85263571+danielbossier@users.noreply.github.com>
2023-09-13 21:25:28 -0700

    Organized Data

--- src/Data.js
+++ src/Data.js
@@ -10,26 +10,6 @@ export const TeamData = [
     },
     {
         id: 2,
-        team_name: "Rams",
-        x: -7,
-        y: -7,
-        image: "/logos/helmetlogos/rams.png",
-        conference: "NFC",
-        division: "West"
-
-    },
-    {
-        id: 3,
-        team_name: "Seahawks",
-        x: 3,
-        y: -10,
-        image: "/logos/helmetlogos/seahawks.png",
-        conference: "NFC",
-        division: "West"
-
-    },
-    {
-        id: 4,
         team_name: "Cardinals",
         x: -10,
         y: -5,
@@ -39,51 +19,71 @@ export const TeamData = [
 
     },
     {
-        id: 5,
-        team_name: "Saints",
-        x: 2,
-        y: 0,
-        image: "/logos/helmetlogos/saints.png",
+        id: 3,
+        team_name: "Rams",
+        x: -7,
+        y: -7,
+        image: "/logos/helmetlogos/rams.png",
         conference: "NFC",
-        division: "South"
-
+        division: "West"
+        
+    },
+    {
+        id: 4,
+        team_name: "Seahawks",
+        x: 3,
+        y: -10,
+        image: "/logos/helmetlogos/seahawks.png",
+        conference: "NFC",
+        division: "West"
+        
+    },
+    {
+        id: 5,
+        team_name: "Bears",
+        x: -5,
+        y: -1,
+        image: "/logos/helmetlogos/bears.png",
+        conference: "NFC",
+        division: "North"
+        
     },
     {
         id: 6,
-        team_name: "Panthers",
-        x: -8,
-        y: 2,
-        image: "/logos/helmetlogos/panthers.png",
+        team_name: "Lions",
+        x: 6,
+        y: 4,
+        image: "/logos/helmetlogos/lions.png",
         conference: "NFC",
-        division: "South"
-
+        division: "North"
+        
     },
     {
         id: 7,
-        team_name: "Buccaneers",
-        x: -4,
-        y: 1,
-        image: "/logos/helmetlogos/buccaneers.png",
+        team_name: "Packers",
+        x: 2,
+        y: 2,
+        image: "/logos/helmetlogos/packers.png",
         conference: "NFC",
-        division: "South"
-
+        division: "North"
+        
     },
     {
         id: 8,
-        team_name: "Falcons",
-        x: -1,
+        team_name: "Vikings",
+        x: 3,
         y: 1,
-        image: "/logos/helmetlogos/falcons.png",
+        image: "/logos/helmetlogos/vikings.png",
         conference: "NFC",
-        division: "South"
+        division: "North"
 
     },
     {
         id: 9,
-        team_name: "Giants",
-        x: 2,
-        y: -5,
-        image: "/logos/helmetlogos/giants.png",
+        team_name: "Commanders",
+        x: -1,
+        y: -3,
+        image: "/logos/helmetlogos/commanders.png",
         conference: "NFC",
         division: "East"
 
@@ -96,7 +96,7 @@ export const TeamData = [
         image: "/logos/helmetlogos/cowboys.png",
         conference: "NFC",
         division: "East"
-
+        
     },
     {
         id: 11,
@@ -106,56 +106,56 @@ export const TeamData = [
         image: "/logos/helmetlogos/eagles.png",
         conference: "NFC",
         division: "East"
-
+        
     },
     {
         id: 12,
-        team_name: "Commanders",
-        x: -1,
-        y: -3,
-        image: "/logos/helmetlogos/commanders.png",
+        team_name: "Giants",
+        x: 2,
+        y: -5,
+        image: "/logos/helmetlogos/giants.png",
         conference: "NFC",
         division: "East"
-
+        
     },
     {
         id: 13,
-        team_name: "Vikings",
-        x: 3,
+        team_name: "Buccaneers",
+        x: -4,
         y: 1,
-        image: "/logos/helmetlogos/vikings.png",
+        image: "/logos/helmetlogos/buccaneers.png",
         conference: "NFC",
-        division: "North"
+        division: "South"
 
     },
     {
         id: 14,
-        team_name: "Packers",
-        x: 2,
-        y: 2,
-        image: "/logos/helmetlogos/packers.png",
+        team_name: "Falcons",
+        x: -1,
+        y: 1,
+        image: "/logos/helmetlogos/falcons.png",
         conference: "NFC",
-        division: "North"
+        division: "South"
 
     },
     {
         id: 15,
-        team_name: "Bears",
-        x: -5,
-        y: -1,
-        image: "/logos/helmetlogos/bears.png",
+        team_name: "Panthers",
+        x: -8,
+        y: 2,
+        image: "/logos/helmetlogos/panthers.png",
         conference: "NFC",
-        division: "North"
+        division: "South"
 
     },
     {
         id: 16,
-        team_name: "Lions",
-        x: 6,
-        y: 4,
-        image: "/logos/helmetlogos/lions.png",
+        team_name: "Saints",
+        x: 2,
+        y: 0,
+        image: "/logos/helmetlogos/saints.png",
         conference: "NFC",
-        division: "North"
+        division: "South"
 
     },
     {
@@ -170,26 +170,6 @@ export const TeamData = [
     },
     {
         id: 18,
-        team_name: "Chiefs",
-        x: 8,
-        y: -4,
-        image: "/logos/helmetlogos/chiefs.png",
-        conference: "AFC",
-        division: "West"
-
-    },
-    {
-        id: 19,
-        team_name: "Raiders",
-        x: -1,
-        y: -1,
-        image: "/logos/helmetlogos/raiders.png",
-        conference: "AFC",
-        division: "West"
-
-    },
-    {
-        id: 20,
         team_name: "Chargers",
         x: 4,
         y: 5,
@@ -199,43 +179,63 @@ export const TeamData = [
 
     },
     {
-        id: 21,
-        team_name: "Colts",
-        x: -7,
-        y: -2,
-        image: "/logos/helmetlogos/colts.png",
+        id: 19,
+        team_name: "Chiefs",
+        x: 8,
+        y: -4,
+        image: "/logos/helmetlogos/chiefs.png",
         conference: "AFC",
-        division: "South"
+        division: "West"
+
+    },
+    {
+        id: 20,
+        team_name: "Raiders",
+        x: -1,
+        y: -1,
+        image: "/logos/helmetlogos/raiders.png",
+        conference: "AFC",
+        division: "West"
+        
+    },
+    {
+        id: 21,
+        team_name: "Bengals",
+        x: 6,
+        y: 6,
+        image: "/logos/helmetlogos/bengals.png",
+        conference: "AFC",
+        division: "North"
 
     },
     {
         id: 22,
-        team_name: "Texans",
-        x: -9,
-        y: -2,
-        image: "/logos/helmetlogos/texans.png",
+        team_name: "Browns",
+        x: 1,
+        y: -7,
+        image: "/logos/helmetlogos/browns.png",
         conference: "AFC",
-        division: "South"
+        division: "North"
 
     },
     {
         id: 23,
-        team_name: "Jaguars",
-        x: 2,
-        y: 3,
-        image: "/logos/helmetlogos/jaguars.png",
+        team_name: "Steelers",
+        x: 4,
+        y: 1,
+        image: "/logos/helmetlogos/steelers.png",
         conference: "AFC",
-        division: "South"
+        division: "North"
 
     },
     {
         id: 24,
-        team_name: "Titans",
-        x: 3,
-        y: -2,
-        image: "/logos/helmetlogos/titans.png",
+        team_name: "Ravens",
+        x: 4,
+        y: 3,
+        image: "/logos/helmetlogos/ravens.png",
         conference: "AFC",
-        division: "South"
+        division: "North"
 
     },
     {
@@ -260,16 +260,6 @@ export const TeamData = [
     },
     {
         id: 27,
-        team_name: "Patriots",
-        x: -2,
-        y: -5,
-        image: "/logos/helmetlogos/patriots.png",
-        conference: "AFC",
-        division: "East"
-
-    },
-    {
-        id: 28,
         team_name: "Jets",
         x: 4,
         y: 2,
@@ -279,43 +269,53 @@ export const TeamData = [
 
     },
     {
-        id: 29,
-        team_name: "Browns",
-        x: 1,
-        y: -7,
-        image: "/logos/helmetlogos/browns.png",
+        id: 28,
+        team_name: "Patriots",
+        x: -2,
+        y: -5,
+        image: "/logos/helmetlogos/patriots.png",
         conference: "AFC",
-        division: "North"
+        division: "East"
+
+    },
+    {
+        id: 29,
+        team_name: "Colts",
+        x: -7,
+        y: -2,
+        image: "/logos/helmetlogos/colts.png",
+        conference: "AFC",
+        division: "South"
 
     },
     {
         id: 30,
-        team_name: "Steelers",
-        x: 4,
-        y: 1,
-        image: "/logos/helmetlogos/steelers.png",
+        team_name: "Jaguars",
+        x: 2,
+        y: 3,
+        image: "/logos/helmetlogos/jaguars.png",
         conference: "AFC",
-        division: "North"
+        division: "South"
 
     },
     {
         id: 31,
-        team_name: "Ravens",
-        x: 4,
-        y: 3,
-        image: "/logos/helmetlogos/ravens.png",
+        team_name: "Texans",
+        x: -9,
+        y: -2,
+        image: "/logos/helmetlogos/texans.png",
         conference: "AFC",
-        division: "North"
+        division: "South"
 
     },
     {
         id: 32,
-        team_name: "Bengals",
-        x: 6,
-        y: 6,
-        image: "/logos/helmetlogos/bengals.png",
+        team_name: "Titans",
+        x: 3,
+        y: -2,
+        image: "/logos/helmetlogos/titans.png",
         conference: "AFC",
-        division: "North"
+        division: "South"
 
     },
 ];
